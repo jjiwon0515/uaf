b4ce4d06d4ac5fe231dc3c5016d4bec255ff4758
장지원 <107084512+jjiwon0515@users.noreply.github.com>
2023-01-05 23:51:29 +0900

    Update practice3.cpp

--- practice_ptr/practice3.cpp
+++ practice_ptr/practice3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cstring>
 
-
+# std 소속에 있는 함수들을 사용할 수 있도록 한다.
 using namespace std;
 
+# 클래스 Car를 선언
 class Car
 {
 
@@ -29,6 +30,9 @@ public:
     void Break();
 };
 
+
+# 클래스 안의 InitMembers 함수들을 가져온다 
+# ID 포인터 변수와 fuel 변수를 매개변수로 받는다.
 void Car::InitMembers(const char* ID, int fuel)
 {
     strcpy_s(gamerID, ID);  // strcpy는 안전하지 않은 함수이므로 strcpy_s 사용
@@ -36,6 +40,8 @@ void Car::InitMembers(const char* ID, int fuel)
     curSpeed = 0;
 }
 
+
+# 클래스 안의 ShowCarstate 함수를 가져온다.
 void Car::ShowCarState()
 {
     cout << "소유자 ID: " << gamerID << endl;
@@ -43,6 +49,8 @@ void Car::ShowCarState()
     cout << "현재 속도: " << curSpeed << "km/h" << endl << endl;
 }
 
+
+# 클래스 안의 Accel 함수를 가져온다.
 void Car::Accel()
 {
     if (fuelGauge <= 0)
@@ -58,6 +66,8 @@ void Car::Accel()
     curSpeed += ACC_STEP;
 }
 
+
+# 클래스 안의 Break 함수들 가져온다.
 void Car::Break()
 {
     if (curSpeed < BRK_STEP)
